97a1dfa91424e1f426133b7842b6bf4ed368c528
AlekZam2 <lex.zam@hotmail.com>
2023-05-18 10:19:27 +0100

    Random number creation

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+import java.util.Random;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Random r = new Random();
+        int result = r.nextInt(7);
+
+        System.out.println(result);
+
+    }
+}
